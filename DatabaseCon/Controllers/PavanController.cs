e380ced892de8e835981688b1ce8e6ae690963c7
STS668-PAVANKUMAR P <STS668-PAVANKUMAR P@STS668L-PAVANKU>
2022-10-18 19:10:25 +0530

    Add project files.

---
+++ DatabaseCon/Controllers/PavanController.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseCon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseCon.Controllers
+{
+    public class PavanController : Controller
+    {
+        // GET: Pavan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Filling()
+        {
+            return View();
+        }
+        public ActionResult Demo(StudentDetails d)
+        {
+            using(var entity = new tablesEntities3())
+            {
+                student t = new student();
+               
+                t.Name = d.Name;
+                t.Email = d.Email;
+                t.Marks = d.Marks;
+                entity.students.Add(t);
+                entity.SaveChanges();
+
+            }
+            return View();
+        }
+    }
+}
